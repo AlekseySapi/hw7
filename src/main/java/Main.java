4f6dc80f3525d3ad6d065ebca82b8ae0bcc2826a
Aleksey L <52339899+AlekseySapi@users.noreply.github.com>
2021-06-23 01:20:33 +0300

    Задание выполнено

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ public class Main {
         либо старницы сайта - вершины, а гиперсылки - рёбра, связывающие вершины.
 */
 
+
 //        Задание 7.2
         Graph graph = new Graph();
         graph.addVertex('A');
@@ -23,6 +24,22 @@ public class Main {
 
         graph.displayVertex(4);
         graph.displayVertex(2);
+
+        System.out.println();
+
+        System.out.println("Реализацию метода обхода в глубину:");
+        long nano_startTime = System.nanoTime();
+        graph.dfs();
+        long nano_endTime = System.nanoTime();
+        System.out.println("Время, затраченное на обход в глубину: " + (nano_endTime - nano_startTime) + " нс");
+
+        System.out.println();
+
+        System.out.println("Реализацию метода обхода в ширину:");
+        nano_startTime = System.nanoTime();
+        graph.bfs();
+        nano_endTime = System.nanoTime();
+        System.out.println("Время, затраченное на обход в ширину: " + (nano_endTime - nano_startTime) + " нс");
 
     }
 
